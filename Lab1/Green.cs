8dfa5dbb8fffce8abf22ee4565aad77160a34eba
Зорина Вера <verazorina71136@gmail.com>
2025-09-16 22:38:33 +0300

    Add files via upload

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            if (Math.Abs(d)>=1) answer = true;
             // end
 
             return answer;
@@ -17,7 +17,7 @@
             bool answer = false;
 
             // code here
-
+            if ((d+f) / 2 >= 0) answer = true;
             // end
 
             return answer;
@@ -27,7 +27,7 @@
             bool answer = false;
 
             // code here
-
+            if(a + b > (Math.Abs(a) + Math.Abs(b)) / 2.0) answer = true;
             // end
 
             return answer;
@@ -37,30 +37,37 @@
             int answer = 0;
 
             // code here
-
+            if (a > answer) answer = a;
+            if (b > answer) answer = b;
+            if (c > answer) answer = c;
             // end
 
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1) answer = 0;
+            else answer = x * x - 1;
             // end
 
-            return answer;
+                return answer;
         }
         public bool Task6(double x, double y)
         {
             bool answer = false;
 
             // code here
-
+            if (y >= 0)
+            {
+                if (x < 0 && y <= 1 + x) answer = true;
+                if (x >= 0 && y <= 1 - x) answer = true;
+            }
             // end
 
-            return answer;
+                return answer;
         }
 
         public bool Task7(int n)
@@ -68,17 +75,24 @@
             bool answer = true;
 
             // code here
-
+            if (n < 0) answer = false;
+            else if (n % 2 == 0) answer = false;
             // end
 
-            return answer;
+                return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
 
             // code here
-
+            int hours = Y + 60;
+            for (int i = 0; i < X; i++)
+            {
+                hours += 60;
+                if (i % 2 == 1) hours += Y;
+            }
+            if (hours >= 240) answer = true;
             // end
 
             return answer;
--- Lab1/White.cs
+++ Lab1/White.cs
@@ -7,7 +7,10 @@
             bool answer = false;
 
             // code here
-
+            if(d>0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +20,7 @@
             bool answer = false;
 
             // code here
-
+            if (n%2==0) {answer = true;}
             // end
 
             return answer;
@@ -27,37 +30,46 @@
             int answer = 0;
 
             // code here
-
+            if (a>b) {answer = a; }
+            else { answer = b; }
             // end
 
-            return answer;
+                return answer;
         }
         public double Task4(double d, double f)
         {
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(d) <= Math.Abs(f)) { answer = d; }
+            else { answer = f; }
             // end
 
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1) { answer = 1; }
+            else { answer = x; }
             // end
 
-            return answer;
+                return answer;
         }
         public bool Task6(double x, double y, double r)
         {
             bool answer = false;
 
             // code here
-
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(r);
+            //Console.WriteLine(Math.Abs(x * x + y * y - r * r));
+            if (Math.Abs(x*x + y*y - r*r) <= 0.000001) 
+            { answer = true; }
+            //if (Math.Sqrt(Math.Pow(x,2) + Math.Pow(y,2)) <= Math.Pow(r,2)) { answer = true; }
             // end
 
             return answer;
@@ -68,6 +80,11 @@
             bool answer = false;
 
             // code here
+            int s = Convert.ToInt32(Math.Pow(n,2));
+            if (s-n>2*n)
+            {
+                if (n%2 == 0) { answer = true; }
+            }
 
             // end
 
@@ -78,7 +95,7 @@
             bool answer = false;
 
             // code here
-
+            if (L/10<=3 && M % 2 == 0 && T+M >= 5) {answer = true; }
             // end
 
             return answer;
